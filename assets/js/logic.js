04e0af0ed0c419137539a770ff6668f9d5300f9a
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-03-08 21:34:50 -0800

    add the buttons

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,32 @@
 // // javascript
 
-$('.closingBtn').on('click', function(){
-    console.log("close");
+// function closing(){
+//     $(this).remove('.closingBtn');
+//     console.log("close from consoolsoslos");
+// }
+
+
+function addButton(btnName) {
+    var newBtn = $('<p>');
+    newBtn.append('<span class="wordBtn">' + btnName + '</span>');
+    newBtn.append('<span onclick="closing()" class="closeBtn closingBtn">X</span>');
+    $("#buttonsPlace").append(newBtn);
+}
+
+
+
+$('#submitBtn').on("click", function (event) {
+    event.preventDefault();
+
+    var animalName = $('#animal').val().trim();
+    if (!(animalName === "")) {
+        addButton(animalName);
+    }
+
+
+    console.log(animalName);
+
+    var form = document.getElementById("animalForm");
+    form.reset();
+
 });
