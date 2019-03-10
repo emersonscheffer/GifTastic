f590d390c1ff9d3d52a1acc17ea2b47cc3701a68
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-03-09 18:08:15 -0800

    better functionallity

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,96 @@
 // // javascript
 
-// function closing(){
-//     $(this).remove('.closingBtn');
-//     console.log("close from consoolsoslos");
+// localStorage.setItem('name', 'Emerson');
+// $('.name').html(localStorage.getItem('name'));
+
+
+//array list of the animals
+var listOfAnimals = [];
+//list of favorites
+var favoritesArr = [];
+// var storedAnimals;
+
+// function favoritesFn() {
+//     favorites.push('horse');
+
+//     localStorage.setItem('favoriteAnimals', JSON.stringify(favorites));
+
+//     storedAnimals = JSON.parse(localStorage.getItem('favoriteAnimals'));
+
+//     populateFavorites();
 // }
 
 
+// function populateFavorites(){
+//     for (var i in storedAnimals) {
+//        // addButtonFavorite(storedAnimals[i]);
+//     }
+// }
+// populateFavorites();
+
+// var names = ['john', 'lucy', 'mark'];
+
+// localStorage.setItem("names", JSON.stringify(names));
+
+// //...
+// var storedNames = JSON.parse(localStorage.getItem("names"));
+
+// console.log(storedNames[0]);
+
+
+
+//background changing
+function setColor(color) {
+
+    localStorage.setItem('bg', color);
+
+    $('body').css('backgroundColor', color);
+
+}
+
+$('body').css('backgroundColor', localStorage.getItem('bg'));
+
+function redBtn() {
+    setColor('red');
+}
+
+function blueBtn() {
+    setColor('blue');
+}
+
+function yellowBtn() {
+    setColor('yellow');
+}
+
+function pinkBtn() {
+    setColor('pink');
+}
+
+function greenBtn() {
+    setColor('green');
+}
+
+
+function favoritesFn(favName) {
+    if (favoritesArr.includes(favName.id)) {
+        ;
+    } else {
+        favoritesArr.push(favName.id);
+    }
+}
+
+
 function addButton(btnName) {
-    var newBtn = $('<p>');
-    newBtn.append('<span class="wordBtn">' + btnName + '</span>');
-    newBtn.append('<span onclick="closing()" class="closeBtn closingBtn">X</span>');
+    var newBtn = $('<p id="' + btnName + '">');
+    newBtn.append('<span class="wordBtn" onclick="generateGifs()">' + btnName + '</span>');
+    newBtn.append('<span alt="Add to Favorites" onclick="favoritesFn(' + btnName + ')" class="closeBtn closingBtn">√</span>');
     $("#buttonsPlace").append(newBtn);
 }
 
+//generate gifs
+function generateGifs() {
+    console.log("Cloe");
+}
 
 
 $('#submitBtn').on("click", function (event) {
@@ -20,11 +98,15 @@ $('#submitBtn').on("click", function (event) {
 
     var animalName = $('#animal').val().trim();
     if (!(animalName === "")) {
-        addButton(animalName);
+        if(listOfAnimals.includes(animalName)){
+            ;
+        } else {
+            listOfAnimals.push(animalName);
+            console.log(listOfAnimals);
+            //call function to add
+            addButton(animalName);
+        }
     }
-
-
-    console.log(animalName);
 
     var form = document.getElementById("animalForm");
     form.reset();
